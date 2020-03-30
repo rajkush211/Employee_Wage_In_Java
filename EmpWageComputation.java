083c4098daf747cf0efba9c9b614d82cac77f0de
rajkush211 <rajkush211.rk@gmail.com>
2020-03-30 08:46:39 +0530

    ADDED: Computing Employee Wage for multiple companies

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -2,23 +2,25 @@ public class EmpWageComputation {
 	//CONSTANTS
 	public static final int IS_PART_TIME = 1;
 	public static final int IS_FULL_TIME = 2;
-	public static final int EMP_RATE_PER_HOUR = 20;
-	public static final int NUM_OF_WORKING_DAYS = 20;
-	public static final int MAX_HRS_IN_MONTH = 100;
+	public static int EMP_RATE_PER_HOUR;
+	public static int NUM_OF_WORKING_DAYS;
+	public static int MAX_HRS_IN_MONTH;
 
-	public static int calTotalEmpWage( int totalEmpHrs )	{
-		return totalEmpHrs * EMP_RATE_PER_HOUR;
+	//CONSTRUCTOR
+	public EmpWageComputation( int EMP_RATE_PER_HOUR, int NUM_OF_WORKING_DAYS, int MAX_HRS_IN_MONTH )	{
+		this.EMP_RATE_PER_HOUR = EMP_RATE_PER_HOUR;
+		this.NUM_OF_WORKING_DAYS = NUM_OF_WORKING_DAYS;
+		this.MAX_HRS_IN_MONTH = MAX_HRS_IN_MONTH;
 	}
 
-	public static void main(String args[]) {
-		//VARIABLES
+	//COMPUTATION
+	public static int computeDailyHrs()	{
 		int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
-		//COMPUTATION
-		while ( totalEmpHrs <= MAX_HRS_IN_MONTH &&
-				totalWorkingDays < NUM_OF_WORKING_DAYS )	{
+		while( totalEmpHrs <= MAX_HRS_IN_MONTH &&
+				totalWorkingDays < NUM_OF_WORKING_DAYS )  {
 			totalWorkingDays++;
 			int empCheck = (int) ( Math.random() * 10 ) % 3;
-			switch (empCheck)	{
+			switch (empCheck) {
 				case IS_PART_TIME:
 					empHrs = 4;
 					break;
@@ -31,7 +33,24 @@ public class EmpWageComputation {
 			totalEmpHrs += empHrs;
 			System.out.println("Day#: " + totalWorkingDays + " Emp Hr: " +  empHrs);
 		}
-		int totalEmpWage = calTotalEmpWage( totalEmpHrs );
-		System.out.println("Total Emp Wage: " + totalEmpWage);
+	return totalEmpHrs;
+}
+	//CALCULATING TOTAL EMPLOYEE WAGE
+	public int calTotalEmpWage( int totalEmpHrs )	{
+		return totalEmpHrs * EMP_RATE_PER_HOUR;
+	}
+
+
+	public static void main(String args[]) {
+		//CREATING OBJECTS FOR COMPANY
+		EmpWageComputation bridgelabz = new EmpWageComputation( 20, 20, 100 );
+		int bridgelabzTotalEmpHrs = bridgelabz.computeDailyHrs();
+		int totalBridgelabzEmpWage = bridgelabz.calTotalEmpWage( bridgelabzTotalEmpHrs );
+		System.out.println("Total Wage of Bridgelabz Employee: " + totalBridgelabzEmpWage);
+
+		EmpWageComputation google = new EmpWageComputation( 50, 15, 75 );
+		int googleTotalEmpHrs = google.computeDailyHrs();
+		int totalGoogleEmpWage = google.calTotalEmpWage( googleTotalEmpHrs );
+		System.out.println("Total Wage of Google Employee: " + totalGoogleEmpWage );
 	}
 }
